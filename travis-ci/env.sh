628c4e3c6dfc7386b477a8833fbd417e66078af9
ShoGinn <2660603+ShoGinn@users.noreply.github.com>
2020-03-04 19:53:34 -0500

    travis tests

--- travis-ci/env.sh
+++ travis-ci/env.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
-export DESCRIPTION="Docker container for ADS-B - This is the adsbhub.org component"
-export REPO_NAME="adsbhub"
+export DOCKER_DESCRIPTION="Docker container for ADS-B - This is the adsbhub.org component"
+export DOCKER_NAME="adsbhub"
